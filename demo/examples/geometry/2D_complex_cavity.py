ac25b2dc8da5f938f04d4507ce07040f9a89d91e
Graille Benjamin <benjamin.graille@math.u-psud.fr>
2015-01-16 15:12:34 +0100

    update geometry: remove list_tag, list_label; simplification of add_elem and visualize (Cf elements) update elements: remove tag, add function _visualize

--- demo/examples/geometry/2D_complex_cavity.py
+++ demo/examples/geometry/2D_complex_cavity.py
@@ -1,29 +1,23 @@
 import pyLBM
 
 if __name__ == "__main__":
-    solid, fluid = 0, 1
-    square = pyLBM.Parallelogram((.1, .1), (.8, 0), (0, .8))
-    strip = pyLBM.Parallelogram((0, .4), (1, 0), (0, .2))
-    circle = pyLBM.Circle((.5, .5), .25)
-    inner_square = pyLBM.Parallelogram((.4, .5), (.1, .1), (.1, -.1))
+    square = pyLBM.Parallelogram((.1, .1), (.8, 0), (0, .8), isfluid=False)
+    strip = pyLBM.Parallelogram((0, .4), (1, 0), (0, .2), isfluid=True)
+    circle = pyLBM.Circle((.5, .5), .25, isfluid=True)
+    inner_square = pyLBM.Parallelogram((.4, .5), (.1, .1), (.1, -.1), isfluid=False)
     dgeom = {
         'box':{'x': [0, 1], 'y': [0, 1], 'label':0},
-        'elements':{
-            0:{'element':square, 'del':solid},
-            1:{'element':strip, 'del':fluid},
-            2:{'element':circle, 'del':fluid},
-            3:{'element':inner_square, 'del':solid},
-        }
+        'elements':[square, strip, circle, inner_square],
     }
     geom = pyLBM.Geometry(dgeom)
     geom.visualize()
     # rounded inner angles
-    geom.add_elem(pyLBM.Parallelogram((0.1, 0.9), (0.05, 0), (0, -0.05)), 0, fluid)
-    geom.add_elem(pyLBM.Circle((0.15, 0.85), 0.05), 0, solid)
-    geom.add_elem(pyLBM.Parallelogram((0.1, 0.1), (0.05, 0), (0, 0.05)), 0, fluid)
-    geom.add_elem(pyLBM.Circle((0.15, 0.15), 0.05), 0, solid)
-    geom.add_elem(pyLBM.Parallelogram((0.9, 0.9), (-0.05, 0), (0, -0.05)), 0, fluid)
-    geom.add_elem(pyLBM.Circle((0.85, 0.85), 0.05), 0, solid)
-    geom.add_elem(pyLBM.Parallelogram((0.9, 0.1), (-0.05, 0), (0, 0.05)), 0, fluid)
-    geom.add_elem(pyLBM.Circle((0.85, 0.15), 0.05), 0, solid)
+    geom.add_elem(pyLBM.Parallelogram((0.1, 0.9), (0.05, 0), (0, -0.05), isfluid=True))
+    geom.add_elem(pyLBM.Circle((0.15, 0.85), 0.05, isfluid=False))
+    geom.add_elem(pyLBM.Parallelogram((0.1, 0.1), (0.05, 0), (0, 0.05), isfluid=True))
+    geom.add_elem(pyLBM.Circle((0.15, 0.15), 0.05, isfluid=False))
+    geom.add_elem(pyLBM.Parallelogram((0.9, 0.9), (-0.05, 0), (0, -0.05), isfluid=True))
+    geom.add_elem(pyLBM.Circle((0.85, 0.85), 0.05, isfluid=False))
+    geom.add_elem(pyLBM.Parallelogram((0.9, 0.1), (-0.05, 0), (0, 0.05), isfluid=True))
+    geom.add_elem(pyLBM.Circle((0.85, 0.15), 0.05, isfluid=False))
     geom.visualize()
--- demo/examples/geometry/2D_square_hole.py
+++ demo/examples/geometry/2D_square_hole.py
@@ -3,7 +3,7 @@ import pyLBM
 if __name__ == "__main__":
     dgeom = {
         'box':{'x': [0, 1], 'y': [0, 1], 'label':0},
-        'elements':{0:{'element':pyLBM.Circle((0.5,0.5), 0.125), 'label':1, 'del':0}},
+        'elements':[pyLBM.Circle((0.5,0.5), 0.125, label = 1)],
     }
     geom = pyLBM.Geometry(dgeom)
-    geom.visualize()
+    geom.visualize(viewlabel=True)
