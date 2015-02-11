28f8447cae30e1a9134fe4fd406996a9787c38dd
gouarin <loic.gouarin@math.u-psud.fr>
2015-02-11 10:03:18 +0100

    - add subarray for communications between processes - add method to update a numpy array on the interfaces

--- pyLBM/interface.py
+++ pyLBM/interface.py
@@ -1,37 +1,121 @@
+import numpy as np
+import mpi4py.MPI as mpi
 
-class interface:
-    def __init__(self, dim, comm, n, vmax, nv_on_beg=False):
-        self.com = com
+class Interface:
+    def __init__(self, dim, comm):
         self.dim = dim
-        self.sendType = []
-        self.recvType = []
+        self.comm = comm
 
+    def set_subarray(self, n, vmax, nv_on_beg=False):
         rank = self.comm.Get_rank()
         coords = self.comm.Get_coords(rank)
-        direction = np.empty((3,)*dim)
-
-        start = []
-        size = []
-        for i in xrange(dim):
-            start.append([vmax[i], vmax[i], n[i]-1])
-            size.append([vmax[i], n[i], vmax[i]])
 
         if nv_on_beg:
+            nloc = [i - 2*v for i, v in zip(n[1:], vmax)]
             nv = n[0]
         else:
+            nloc = [i - 2*v for i, v in zip(n[:-1], vmax)]
             nv = n[-1]
 
-        if dim == 2:
-            for i, vi in enumerate([-1, 0, 1]):
-                for j, vj in enumerate([-1, 0, 1]):
-                    if i != 0 and j != 0:
-                    neighbor = comm.Get_cart_rank(coords + np.array([vi, vj]))
-                    self.neighbors.append(neighbor)
-                    if nv_on_beg:
-                        ss = [nv, size[j], size[i]]
-                        st = [nv, start[j], start[i]]
-                    else:
-                        ss = [size[j], size[i], nv]
-                        st = [start[j], start[i], nv]
+        start_send = []
+        start_recv = []
+        msize = []
+        stag = np.arange((3)**self.dim).reshape((3,)*self.dim)
+        rtag = stag[::-1, ::-1]
+        for i in xrange(self.dim):
+            start_send.append([vmax[i], vmax[i], n[i]-2*vmax[i]])
+            start_recv.append([0, vmax[i], n[i]-vmax[i]])
+            msize.append([vmax[i], nloc[i], vmax[i]])
+        start_send = np.asarray(start_send)
+        start_recv = np.asarray(start_recv)
+        msize = np.asarray(msize)
 
-                    self.sendType.append(mpi.DOUBLE.Create_subarray(n, ss, st))
+        self.neighbors = []
+        self.sendType = []
+        self.sendTag = []
+        self.recvType = []
+        self.recvTag = []
+        directions = get_directions(self.dim)
+        rows = np.arange(self.dim)
+        for d in directions:
+            if not np.all(d == 0):
+                try:
+                    neighbor = self.comm.Get_cart_rank(coords + d)
+                    self.neighbors.append(neighbor)
+
+                    if nv_on_beg:
+                        ms = [nv] + list(msize[rows, d[::-1]+1])
+                        ss = [0] + list(start_send[rows, d[::-1]+1])
+                        sr = [0] + list(start_recv[rows, d[::-1]+1])
+                    else:
+                        ms = list(msize[rows, d[::-1]+1]) + [nv]
+                        ss = list(start_send[rows, d[::-1]+1]) + [0]
+                        sr = list(start_recv[rows, d[::-1]+1]) + [0]
+
+                    self.sendType.append(mpi.DOUBLE.Create_subarray(n, ms, ss))
+                    self.recvType.append(mpi.DOUBLE.Create_subarray(n, ms, sr))
+                    self.sendTag.append(stag[d[0]+1, d[1]+1])
+                    self.recvTag.append(rtag[d[0]+1, d[1]+1])
+                    print rank, neighbor, ms, ss, sr
+                except mpi.Exception:
+                    pass
+
+        for s, r in zip(self.sendType, self.recvType):
+            s.Commit()
+            r.Commit()
+
+    def update(self, f):
+        req = []
+
+        for i in xrange(len(self.recvType)):
+            req.append(mpi.COMM_WORLD.Irecv([f, self.recvType[i]], source = self.neighbors[i], tag=self.recvTag[i]))
+
+        for i in xrange(len(self.sendType)):
+            req.append(mpi.COMM_WORLD.Isend([f, self.sendType[i]], dest = self.neighbors[i], tag=self.sendTag[i]))
+
+        mpi.Request.Waitall(req)
+
+def get_directions(dim):
+    a = np.array([-1, 0, 1])
+
+    if dim == 1:
+        directions = a[:, np.newaxis]
+    elif dim == 2:
+        a = a[np.newaxis, :]
+
+        directions = np.empty((9, 2), dtype=np.int32)
+        directions[:, 0] = np.repeat(a, 3, axis=1).flatten()
+        directions[:, 1] = np.repeat(a, 3, axis=0).flatten()
+    elif dim == 3:
+        a = a[np.newaxis, :]
+
+        directions = np.empty((27, 3), dtype=np.int32)
+        directions[:, 0] = np.repeat(a, 9, axis=1).flatten()
+        directions[:, 1] = np.repeat(np.repeat(a, 3, axis=0), 3).flatten()
+        directions[:, 2] = np.repeat(a, 9, axis=0).flatten()
+
+    return directions
+
+if __name__ == '__main__':
+    dim = 2
+    period = [False]*dim
+    comm = mpi.COMM_WORLD
+    rank = comm.Get_rank()
+
+    n = [10, 10, 2]
+    vmax = [2, 2]
+
+    f = rank*np.ones(n)
+
+    print rank, f[:, :, 0]
+
+    comm = mpi.COMM_WORLD
+    size = comm.Get_size()
+    split = mpi.Compute_dims(size, dim)
+    comm = comm.Create_cart(split, period)
+
+    i = Interface(dim, comm, n, vmax)
+
+    i.update(f)
+
+    print rank, f[:, :, 0].transpose()
