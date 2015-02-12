a0f0e57c26827ae7b55a1497587c4c3c6f355786
gouarin <loic.gouarin@math.u-psud.fr>
2015-02-12 12:55:25 +0100

    add documentation in interface.py

--- pyLBM/interface.py
+++ pyLBM/interface.py
@@ -1,26 +1,90 @@
+# Authors:
+#     Loic Gouarin <loic.gouarin@math.u-psud.fr>
+#     Benjamin Graille <benjamin.graille@math.u-psud.fr>
+#
+# License: BSD 3 clause
+
 import numpy as np
 import mpi4py.MPI as mpi
 from argparse import ArgumentParser
 
+from .logs import __setLogger
+log = __setLogger(__name__)
+
 class Interface:
+    """
+    Interface construction using a MPI topology.
+
+    Parameters
+    ----------
+
+    dim : int
+      number of spatial dimensions (1, 2, or 3)
+    period : list
+      boolean list that specifies if a direction is periodic or not.
+      Its size is dim.
+
+    Attributes
+    ----------
+
+    dim : int
+      number of spatial dimensions (1, 2, or 3)
+    comm : MPI communicator
+      the communicator of the topology
+    split : tuple
+      number of processes in each direction
+    neighbors : list
+      list of the neighbors where we have to send and to receive messages
+    sendType : list
+      list of subarrays that defines the part of data to be send
+    sendTag : list
+      list of tags for the send messages
+    recvType : list
+      list of subarrays that defines the part of data to update during a receive message
+    recvTag : list
+      list of tags for the receive messages
+
+    Methods
+    -------
+
+    set_options :
+      defines command line options.
+    get_coords :
+      return the coords of the process in the MPI topology.
+    set_subarray :
+      create subarray for the send and receive message
+    update :
+      update a numpy array according to the subarrays and the topology.
+
+    """
     def __init__(self, dim, period):
         self.dim = dim
         self.set_options()
 
         comm = mpi.COMM_WORLD
+        # if npx, npy and npz are all the default value (1)
+        # then Compute_dims performs the splitting of the domain
         if self.npx == self.npy == self.npz == 1:
             size = comm.Get_size()
             split = mpi.Compute_dims(size, self.dim)
         else:
             split = (self.npx, self.npy, self.npz)
+
         self.split = split[:self.dim]
         self.comm = comm.Create_cart(self.split, period)
 
     def get_coords(self):
+        """
+        return the coords of the process in the MPI topology
+        as a numpy array.
+        """
         rank = self.comm.Get_rank()
         return np.asarray(self.comm.Get_coords(rank))
 
     def set_options(self):
+        """
+        defines command line options.
+        """
         parser = ArgumentParser()
         parser.add_argument("-npx", dest="npx", default=1,
                              help="Set the number of processes in x direction")
@@ -34,9 +98,27 @@ class Interface:
         self.npz = int(args.npz)
 
     def set_subarray(self, n, vmax, nv_on_beg=False):
+        """
+        Create the neigbors and the subarrays to update interfaces
+        between each processes.
+
+        Parameters
+        ----------
+
+        n : list
+          shape of the moment and distribution arrays
+        vmax : list
+          the maximal velocity in norm for each spatial direction.
+        nv_on_beg : boolean
+          True if the LBM velocities are set on the beginning of the moment
+          and distribution arrays and False otherwise.
+
+        """
+
         rank = self.comm.Get_rank()
         coords = self.comm.Get_coords(rank)
 
+        # set nloc without the ghost points
         if nv_on_beg:
             nloc = [i - 2*v for i, v in zip(n[1:], vmax)]
             nv = n[0]
@@ -44,6 +126,8 @@ class Interface:
             nloc = [i - 2*v for i, v in zip(n[:-1], vmax)]
             nv = n[-1]
 
+        # set the size and the start indices
+        # for the send and receive messages
         start_send = []
         start_recv = []
         msize = []
@@ -57,6 +141,8 @@ class Interface:
         start_recv = np.asarray(start_recv)
         msize = np.asarray(msize)
 
+        # set the neighbors of the domain and their subarrays
+        # for the send and receive messages
         self.neighbors = []
         self.sendType = []
         self.sendTag = []
@@ -83,8 +169,8 @@ class Interface:
                     self.recvType.append(mpi.DOUBLE.Create_subarray(n, ms, sr))
                     self.sendTag.append(stag[d[0]+1, d[1]+1])
                     self.recvTag.append(rtag[d[0]+1, d[1]+1])
-                    print "[{0}] send to {1} with tag {2} subarray:{3}".format(rank, neighbor, self.sendTag[-1], (n, ms, ss))
-                    print "[{0}] recv from {1} with tag {2} subarray:{3}".format(rank, neighbor, self.recvTag[-1], (n, ms, sr))
+                    log.info("[{0}] send to {1} with tag {2} subarray:{3}".format(rank, neighbor, self.sendTag[-1], (n, ms, ss)))
+                    log.info("[{0}] recv from {1} with tag {2} subarray:{3}".format(rank, neighbor, self.recvTag[-1], (n, ms, sr)))
                 except mpi.Exception:
                     pass
 
@@ -93,6 +179,9 @@ class Interface:
             r.Commit()
 
     def update(self, f):
+        """
+        update ghost points on the interface with the datas of the neighbors.
+        """
         req = []
 
         for i in xrange(len(self.recvType)):
@@ -104,6 +193,34 @@ class Interface:
         mpi.Request.Waitall(req)
 
 def get_directions(dim):
+    """
+    Return an array with all the directions around.
+
+    Parameters
+    ----------
+
+    dim : int
+      number of spatial dimensions (1, 2, or 3)
+
+    Examples
+    --------
+
+    >>> get_directions(1)
+    array([[-1],
+       [ 0],
+       [ 1]])
+    >>> get_directions(2)
+    array([[-1, -1],
+       [-1,  0],
+       [-1,  1],
+       [ 0, -1],
+       [ 0,  0],
+       [ 0,  1],
+       [ 1, -1],
+       [ 1,  0],
+       [ 1,  1]], dtype=int32)
+
+    """
     a = np.array([-1, 0, 1])
 
     if dim == 1:
@@ -123,27 +240,3 @@ def get_directions(dim):
         directions[:, 2] = np.repeat(a, 9, axis=0).flatten()
 
     return directions
-
-if __name__ == '__main__':
-    dim = 2
-    period = [False]*dim
-    comm = mpi.COMM_WORLD
-    rank = comm.Get_rank()
-
-    n = [10, 10, 2]
-    vmax = [2, 2]
-
-    f = rank*np.ones(n)
-
-    print rank, f[:, :, 0]
-
-    comm = mpi.COMM_WORLD
-    size = comm.Get_size()
-    split = mpi.Compute_dims(size, dim)
-    comm = comm.Create_cart(split, period)
-
-    i = Interface(dim, comm, n, vmax)
-
-    i.update(f)
-
-    print rank, f[:, :, 0].transpose()
