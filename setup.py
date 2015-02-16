7b35b10692a1845f7233cc6589ea62c595004516
Graille Benjamin <benjamin.graille@math.u-psud.fr>
2015-02-16 14:07:24 +0100

    setup with setuptools

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
-from distutils.core import setup, Extension
-from Cython.Distutils import build_ext
+from setuptools import setup, Extension
+#from distutils.core import setup, Extension
+#from Cython.Distutils import build_ext
 import numpy
 
 setup(
@@ -10,7 +11,7 @@ setup(
     author_email   = "benjamin.graille@math.u-psud.fr, loic.gouarin@math.u-psud.fr",
     packages       = ['pyLBM'],
     package_data   = {'pyLBM': ['../tests/data/domain/*']},
-    cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("pyLBM.bc_utils", ["pyLBM/bc_utils.pyx"],)],
+    #cmdclass = {'build_ext': build_ext},
+    #ext_modules = [Extension("pyLBM.bc_utils", ["pyLBM/bc_utils.pyx"],)],
     setup_requires = ['Cython', 'sphinx'],
 )
