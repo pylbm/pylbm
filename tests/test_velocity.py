4c002cb330d8339830ca2158a14ebaadf77506aa
gouarin <loic.gouarin@math.u-psud.fr>
2015-05-03 09:30:52 +0200

    fix test_velocity

--- tests/test_velocity.py
+++ tests/test_velocity.py
@@ -10,12 +10,12 @@ class test_velocity_1D:
     num = [0, 1, 2, 3, 4, 5, 6, 7]
     vx = [0, 1, -1, 2, -2, 3, -3, 4]
 
-    def test_with_num(self): 
+    def test_with_num(self):
         for n, vx in zip(self.num, self.vx):
             v = Velocity(dim=1, num=n)
             assert(v.vx == vx)
 
-    def test_with_vx(self): 
+    def test_with_vx(self):
         for n, vx in zip(self.num, self.vx):
             v = Velocity(vx=vx)
             assert(v.num == n and v.dim == 1)
@@ -40,33 +40,34 @@ class test_velocity_2D:
     snum = [0, 3, 4, 1, 2, 7, 8, 5, 6]
     vx = [0, 1, 0, -1,  0, 1, -1, -1,  1]
     vy = [0, 0, 1,  0, -1, 1,  1, -1, -1]
+    nrep = 10
 
-    def test_with_num(self): 
+    def test_with_num(self):
         for n, vx, vy in zip(self.num, self.vx, self.vy):
             v = Velocity(dim=2, num=n)
             assert(v.vx == vx and v.vy == vy)
 
-    def test_with_v(self): 
+    def test_with_v(self):
         for n, vx, vy in zip(self.num, self.vx, self.vy):
             v = Velocity(vx=vx, vy=vy)
             assert(v.num == n and v.dim == 2)
 
     def test_symmetry1(self):
-        for i in xrange(10):
+        for i in xrange(self.nrep):
             n = random.randint(0,1000)
             v = Velocity(dim=2, num=n)
             vs = v.get_symmetric().get_symmetric()
             assert(vs.vx == v.vx and vs.vy == v.vy and vs.num == v.num)
 
     def test_symmetry2(self):
-        for i in xrange(10):
+        for i in xrange(self.nrep):
             n = random.randint(0,1000)
             v = Velocity(dim=2, num=n)
             vs = v.get_symmetric(axis=0).get_symmetric(axis=0)
             assert(vs.vx == v.vx and vs.vy == v.vy and vs.num == v.num)
 
     def test_symmetry3(self):
-        for i in xrange(10):
+        for i in xrange(self.nrep):
             n = random.randint(0,1000)
             v = Velocity(dim=2, num=n)
             vs = v.get_symmetric(axis=0).get_symmetric(axis=1)
@@ -80,48 +81,48 @@ class test_velocity_2D:
             assert(vs.num == sn)
 
     @tools.raises(ValueError)
-    def test_symmetry3(self):
+    def test_symmetry5(self):
         v = Velocity(dim=2, num=0)
         vs = v.get_symmetric(axis=2).get_symmetric(axis=2)
 
 class test_velocity_3D:
     num = range(27)
+    nrep = 10
     vx = [0, 0, 0, 0, 0, 1,-1, 0, 0, 0, 0, 1, 1,-1,-1, 1, 1,-1,-1, 1, 1, 1, 1,-1,-1,-1,-1]
     vy = [0, 0, 0, 1,-1, 0, 0, 1, 1,-1,-1, 0, 0, 0, 0, 1,-1, 1,-1, 1, 1,-1,-1, 1, 1,-1,-1]
     vz = [0, 1,-1, 0, 0, 0, 0, 1,-1, 1,-1, 1,-1, 1,-1, 0, 0, 0, 0, 1,-1, 1,-1, 1,-1, 1,-1]
 
-    def test_with_num(self): 
+    def test_with_num(self):
         for n, vx, vy, vz in zip(self.num, self.vx, self.vy, self.vz):
             v = Velocity(dim=3, num=n)
             assert(v.vx == vx and v.vy == vy and v.vz == vz)
 
-    def test_with_v(self): 
+    def test_with_v(self):
         for n, vx, vy, vz in zip(self.num, self.vx, self.vy, self.vz):
             v = Velocity(vx=vx, vy=vy, vz=vz)
             assert(v.num == n and v.dim == 3)
 
     def test_symmetry1(self):
-        for i in xrange(10):
+        for i in xrange(self.nrep):
             n = random.randint(0,1000)
             v = Velocity(dim=3, num=n)
             vs = v.get_symmetric().get_symmetric()
-            assert(vs.vx == v.vx and vs.vy == v.vy 
+            assert(vs.vx == v.vx and vs.vy == v.vy
                    and vs.vz == v.vz and vs.num == v.num)
 
     def test_symmetry2(self):
-        for i in xrange(10):
+        for i in xrange(self.nrep):
             n = random.randint(0,1000)
             v = Velocity(dim=3, num=n)
             vs = v.get_symmetric(axis=0).get_symmetric(axis=0)
-            assert(vs.vx == v.vx and vs.vy == v.vy and 
+            assert(vs.vx == v.vx and vs.vy == v.vy and
                    vs.vz == v.vz and vs.num == v.num)
 
     def test_symmetry3(self):
-        for i in xrange(10):
+        for i in xrange(self.nrep):
             n = random.randint(0,1000)
             v = Velocity(dim=3, num=n)
-            vs = v.get_symmetric(axis=0).get_symmetric(axis=1)
-            vs = vs.get_symmetric(axis=0).get_symmetric(axis=1)
-            assert(vs.vx == v.vx and vs.vy == v.vy and 
+            vs = v.get_symmetric(axis=0).get_symmetric(axis=1).get_symmetric(axis=2)
+            vs = vs.get_symmetric(axis=0).get_symmetric(axis=1).get_symmetric(axis=2)
+            assert(vs.vx == v.vx and vs.vy == v.vy and
                    vs.vz == v.vz and vs.num == v.num)
-
