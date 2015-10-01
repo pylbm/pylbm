361eed0d27ee68a383deff86c5f433800e6dfaf9
Graille Benjamin <benjamin.graille@math.u-psud.fr>
2015-10-01 09:58:34 +0200

    example of the domain "cube with a hole"

---
+++ demo/examples/domain/3D_cube_hole.py
@@ -0,0 +1,19 @@
+# Authors:
+#     Loic Gouarin <loic.gouarin@math.u-psud.fr>
+#     Benjamin Graille <benjamin.graille@math.u-psud.fr>
+#
+# License: BSD 3 clause
+
+"""
+Example of the cube in 3D
+"""
+import pyLBM
+dico = {
+    'box':{'x': [0, 2], 'y': [0, 2], 'z':[0, 2], 'label':range(1,7)},
+    'elements':[pyLBM.Sphere((0.5,0.5,0.5), 0.2, label = 0)],
+    'space_step':1,
+    'schemes':[{'velocities':range(19)}]
+}
+dom = pyLBM.Domain(dico)
+print dom
+dom.visualize(view_distance=True)
