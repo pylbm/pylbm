e85348293ddd1a1f7ff4bb3948c218f8e1145fa4
gouarin <loic.gouarin@gmail.com>
2017-03-21 14:25:02 +0100

    fix context to not import pyopencl if we don't use loopy

--- pyLBM/context.py
+++ pyLBM/context.py
@@ -1,8 +1,13 @@
-try:
-    import pyopencl as cl
-    import pyopencl.array
 
-    ctx = cl.create_some_context()
-    queue = cl.CommandQueue(ctx)
-except:
-    queue = None
+queue = None
+
+def set_queue(backend):
+    global queue
+    if not init_queue:
+        init_queue = True
+        if backend.upper() == "LOOPY":
+            import pyopencl as cl
+            import pyopencl.array
+
+            ctx = cl.create_some_context()
+            queue = cl.CommandQueue(ctx)
