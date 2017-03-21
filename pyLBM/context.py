6b97c99b281ddae1dce7af7f2338c485bf4676f0
gouarin <loic.gouarin@gmail.com>
2017-03-21 16:49:47 +0100

    fix context

--- pyLBM/context.py
+++ pyLBM/context.py
@@ -3,11 +3,9 @@ queue = None
 
 def set_queue(backend):
     global queue
-    if not init_queue:
-        init_queue = True
-        if backend.upper() == "LOOPY":
-            import pyopencl as cl
-            import pyopencl.array
+    if backend.upper() == "LOOPY":
+        import pyopencl as cl
+        import pyopencl.array
 
-            ctx = cl.create_some_context()
-            queue = cl.CommandQueue(ctx)
+        ctx = cl.create_some_context()
+        queue = cl.CommandQueue(ctx)
