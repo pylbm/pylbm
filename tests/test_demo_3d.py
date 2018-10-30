36a7590236231fe11f3211557c06cd2361be6e07
gouarin <loic.gouarin@gmail.com>
2018-10-30 19:29:41 +0100

    fix test demo names in order to have different output names for the reference

--- tests/test_demo_3d.py
+++ tests/test_demo_3d.py
@@ -10,32 +10,32 @@ path = os.path.abspath(path)
 
 
 @pytest.fixture
-def test_case_dir():
+def test3D_case_dir():
     sys.path.append(path)
     yield
     sys.path.pop()
 
 @pytest.mark.slow
 @pytest.mark.h5diff(single_reference=True)
-@pytest.mark.usefixtures("test_case_dir")
+@pytest.mark.usefixtures("test3D_case_dir")
 @pytest.mark.parametrize('generator', ['numpy', 'cython'])
-class Test1D:
+class Test3D:
     def runtest(self, dx, Tf, name, generator):
         module = importlib.import_module(name)
         return module.run(dx, Tf, generator=generator, withPlot=False)
 
-    def test_advection(self, generator):
+    def test3D_advection(self, generator):
         dx, Tf = 1./64, 0.5
         return self.runtest(dx, Tf, 'advection', generator)
 
-    def test_karman(self, generator):
+    def test3D_karman(self, generator):
         dx, Tf = 1./64, 0.5
         return self.runtest(dx, Tf, 'Karman', generator)
 
-    def test_lid_cavity(self, generator):
+    def test3D_lid_cavity(self, generator):
         dx, Tf = 1./64, 0.5
         return self.runtest(dx, Tf, 'lid_cavity', generator)
 
-    def test_poseuille(self, generator):
+    def test3D_poseuille(self, generator):
         dx, Tf = 1./64, 0.5
         return self.runtest(dx, Tf, 'poiseuille', generator)        
