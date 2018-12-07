48b7b6099b70801883965bb04cb8ec8cbb8869c3
Graille <benjamin.graille@math.u-psud.fr>
2018-12-07 23:47:53 +0100

    demo 1D hyperbolique (advection, burgers, shallow water, p-system)

--- demo/1D/riemann_problems/p_system.py
+++ demo/1D/riemann_problems/p_system.py
@@ -1,70 +1,45 @@
 
 
 """
- Solver D1Q2Q2 for the shallow water system on [0, 1]
+ Solver D1Q2Q2 for the p-system on [0, 1]
 
- d_t(h) + d_x(q)    = 0, t > 0, 0 < x < 1,
- d_t(q) + d_x(q^2/h+gh^2/2) = 0, t > 0, 0 < x < 1,
- h(t=0,x) = h0(x), q(t=0,x) = q0(x),
- d_t(h)(t,x=0) = d_t(h)(t,x=1) = 0
- d_t(q)(t,x=0) = d_t(q)(t,x=1) = 0
+ d_t(u1) - d_x(u2)    = 0, t > 0, 0 < x < 1,
+ d_t(u2) - d_x(p(u1)) = 0, t > 0, 0 < x < 1,
+ u1(t=0,x) = u1_0(x), u2(t=0,x) = u2_0(x),
+ d_t(u1)(t,x=0) = d_t(u1)(t,x=1) = 0
+ d_t(u2)(t,x=0) = d_t(u2)(t,x=1) = 0
 
  the initial condition is a picewise constant function
  in order to visualize the simulation of elementary waves
 
  test: True
 """
+
 import sympy as sp
-import numpy as np
 import pylbm
-from riemann_solvers import ShallowWaterSolver as exact_solver
+from exact_solvers import PSystemSolver as exact_solver
+from exact_solvers import riemann_pb
 
-H, Q, X = sp.symbols('h, q, X')
-LA, G = sp.symbols('lambda, g', constants=True)
-SIGMA_H, SIGMA_Q = sp.symbols('sigma_1, sigma_2', constants=True)
+# pylint: disable=redefined-outer-name
 
-
-def riemann_pb(x, xmid, u_left, u_right):
-    """
-    initial condition with a Riemann problem
-
-    Parameters
-    ----------
-
-    x : ndarray
-        spatial mesh
-
-    xmid : double
-        position of the discontinuity
-
-    u_left : double
-        left value of the field
-
-    u_right : double
-        right value of the field
-
-    Returns
-    -------
-
-    vect_u
-        ndarray
-    """
-    vect_u = np.empty(x.shape)
-    vect_u[x < xmid] = u_left
-    vect_u[x == xmid] = .5*(u_left+u_right)
-    vect_u[x > xmid] = u_right
-    return vect_u
+U1, U2, X = sp.symbols('u_1, u_2, X')
+LA, SIGMA_1, SIGMA_2 = sp.symbols('lambda, sigma_1, sigma_2', constants=True)
+GAMMA = sp.Symbol('gamma', constants=True)
 
 
-def run(dx, finaltime, generator="cython", sorder=None, with_plot=True):
+def run(space_step,
+        final_time,
+        generator="cython",
+        sorder=None,
+        with_plot=True):
     """
     Parameters
     ----------
 
-    dx: double
+    space_step: double
         spatial step
 
-    finaltime: double
+    final_time: double
         final time
 
     generator: string
@@ -85,49 +60,50 @@ def run(dx, finaltime, generator="cython", sorder=None, with_plot=True):
 
     """
     # parameters
-    xmin, xmax = -1, 1.        # bounds of the domain
-    gnum = 1                   # numerical value of g
-    la = 2.                    # velocity of the scheme
-    s_h, s_u = 1.5, 1.5        # relaxation parameters
+    xmin, xmax = 0., 1.  # bounds of the domain
+    gamma = 2./3.        # exponent in the p-function
+    la = 2.              # velocity of the scheme
+    s_1, s_2 = 1.9, 1.9  # relaxation parameter
 
-    symb_s_h = 1/(.5+SIGMA_H)  # symbolic relaxation parameters
-    symb_s_q = 1/(.5+SIGMA_Q)  # symbolic relaxation parameters
+    symb_s_1 = 1/(.5+SIGMA_1)  # symbolic relaxation parameters
+    symb_s_2 = 1/(.5+SIGMA_2)  # symbolic relaxation parameters
 
     # initial values
-    h_left, h_right, q_left, q_right = 2, 1, .1, 0.
+    u1_left, u1_right, u2_left, u2_right = 1.50, 0.50, 1.25, 1.50
     # fixed bounds of the graphics
-    ymina, ymaxa = 0.9, 2.1
-    yminb, ymaxb = -.1, .7
+    ymina, ymaxa, yminb, ymaxb = .25, 1.75, 0.75, 1.75
     # discontinuity position
     xmid = .5*(xmin+xmax)
 
     exact_solution = exact_solver({
         'jump abscissa': xmid,
-        'left state': [h_left, q_left],
-        'right state': [h_right, q_right],
-        'g': gnum,
+        'left state': [u1_left, u2_left],
+        'right state': [u1_right, u2_right],
+        'gamma': gamma,
     })
+
+    exact_solution.diagram()
 
     simu_cfg = {
         'box': {'x': [xmin, xmax], 'label': 0},
-        'space_step': dx,
+        'space_step': space_step,
         'scheme_velocity': LA,
         'schemes': [
             {
                 'velocities': [1, 2],
-                'conserved_moments': H,
+                'conserved_moments': U1,
                 'polynomials': [1, X],
-                'relaxation_parameters': [0, symb_s_h],
-                'equilibrium': [H, Q],
-                'init': {H: (riemann_pb, (xmid, h_left, h_right))},
+                'relaxation_parameters': [0, symb_s_1],
+                'equilibrium': [U1, -U2],
+                'init': {U1: (riemann_pb, (xmid, u1_left, u1_right))},
             },
             {
                 'velocities': [1, 2],
-                'conserved_moments': Q,
+                'conserved_moments': U2,
                 'polynomials': [1, X],
-                'relaxation_parameters': [0, symb_s_q],
-                'equilibrium': [Q, Q**2/H+.5*G*H**2],
-                'init': {Q: (riemann_pb, (xmid, q_left, q_right))},
+                'relaxation_parameters': [0, symb_s_2],
+                'equilibrium': [U2, U1**(-GAMMA)],
+                'init': {U2: (riemann_pb, (xmid, u2_left, u2_right))},
             },
         ],
         'boundary_conditions': {
@@ -139,9 +115,9 @@ def run(dx, finaltime, generator="cython", sorder=None, with_plot=True):
         'generator': generator,
         'parameters': {
             LA: la,
-            G: gnum,
-            SIGMA_H: 1/s_h-.5,
-            SIGMA_Q: 1/s_u-.5,
+            GAMMA: gamma,
+            SIGMA_1: 1/s_1-.5,
+            SIGMA_2: 1/s_2-.5,
         },
     }
 
@@ -156,15 +132,15 @@ def run(dx, finaltime, generator="cython", sorder=None, with_plot=True):
         viewer = pylbm.viewer.matplotlib_viewer
         fig = viewer.Fig(2, 1)
         ax1 = fig[0]
-        ax1.axis(xmin, xmax, .9*ymina, 1.1*ymaxa)
-        ax1.set_label(None, "height")
+        ax1.axis(xmin, xmax, ymina, ymaxa)
+        ax1.set_label(None, r"$u_1$")
         ax1.xaxis_set_visible(False)
         ax2 = fig[1]
-        ax2.axis(xmin, xmax, .9*yminb, 1.1*ymaxb)
-        ax2.set_label(r"$x$", "velocity")
+        ax2.axis(xmin, xmax, yminb, ymaxb)
+        ax2.set_label(r"$x$", r"$u_2$")
 
         x = sol.domain.x
-        l1a = ax1.plot(x, sol.m[H],
+        l1a = ax1.plot(x, sol.m[U1],
                        color='navy',
                        alpha=0.5,
                        label=r'$D_1Q_2$',
@@ -175,12 +151,12 @@ def run(dx, finaltime, generator="cython", sorder=None, with_plot=True):
                        alpha=0.5,
                        label='exact',
                        )[0]
-        l2a = ax2.plot(x, sol.m[Q]/sol.m[H],
+        l2a = ax2.plot(x, sol.m[U2],
                        color='orange',
                        alpha=0.5,
                        label=r'$D_1Q_2$',
                        )[0]
-        l2e = ax2.plot(x, sole[1]/sole[0], width=1,
+        l2e = ax2.plot(x, sole[1], width=1,
                        color='black',
                        alpha=0.5,
                        label='exact',
@@ -195,10 +171,10 @@ def run(dx, finaltime, generator="cython", sorder=None, with_plot=True):
                    )
 
         def update(iframe):  # pylint: disable=unused-argument
-            if sol.t < finaltime:
+            if sol.t < final_time:
                 sol.one_time_step()
-                l1a.set_data(x, sol.m[H])
-                l2a.set_data(x, sol.m[Q])
+                l1a.set_data(x, sol.m[U1])
+                l2a.set_data(x, sol.m[U2])
                 sole = exact_solution.evaluate(x, sol.t)
                 l1e.set_data(x, sole[0])
                 l2e.set_data(x, sole[1])
@@ -207,7 +183,7 @@ def run(dx, finaltime, generator="cython", sorder=None, with_plot=True):
         fig.animate(update)
         fig.show()
     else:
-        while sol.t < finaltime:
+        while sol.t < final_time:
             sol.one_time_step()
 
     return sol
@@ -215,5 +191,5 @@ def run(dx, finaltime, generator="cython", sorder=None, with_plot=True):
 if __name__ == '__main__':
     # pylint: disable=invalid-name
     space_step = 1./256
-    final_time = .5
+    final_time = .25
     solution = run(space_step, final_time, generator="numpy")
