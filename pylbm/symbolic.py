46887e36de802bd07cdc36d98a9139020ca559a2
gouarin <loic.gouarin@gmail.com>
2019-02-05 16:49:16 +0100

    add documentation for algorithm fix naming of methods

--- pylbm/symbolic.py
+++ pylbm/symbolic.py
@@ -10,52 +10,183 @@ Symbolic module
 
 import sys
 import inspect
+import numpy as np
 import sympy as sp
 
+# List of symbols used in pylbm
 nx, ny, nz, nv = sp.symbols("nx, ny, nz, nv", integer=True) #pylint: disable=invalid-name
 ix, iy, iz, iv = sp.symbols("ix, iy, iz, iv", integer=True) #pylint: disable=invalid-name
 ix_, iy_, iz_, iv_ = sp.symbols("ix_, iy_, iz_, iv_", integer=True) #pylint: disable=invalid-name
 rel_ux, rel_uy, rel_uz = sp.symbols('rel_ux, rel_uy, rel_uz', real=True) #pylint: disable=invalid-name
 
-def set_order(array, sorder, remove_ind=None):
-    out = [-1]*len(sorder)
-    for i, s in enumerate(sorder):
-        out[s] = array[i]
-    if remove_ind:
-        for i in remove_ind:
-            out.pop(sorder[i])
-    return out
 
-def indexed(name, shape, index=(iv, ix, iy, iz), list_ind=None, ranges=None, permutation=None, remove_ind=None):
-    if not permutation:
-        permutation = range(len(index))
+def set_order(array, priority=None):
+    """
+    Reorder an array according to priority (lower to greater).
 
-    output = sp.IndexedBase(name, set_order(shape, permutation, remove_ind))
+    Parameters
+    ----------
 
-    if ranges:
-        ind = [set_order([k] + index[1:], permutation, remove_ind) for k in ranges]
+    array : list
+        array to reorder
+
+    priority : list
+        define how to reorder (lower to greater)
+        (default is None)
+
+    Return
+    ------
+
+    list
+        new reordered list
+    """
+    if priority:
+        out = []
+        for p in np.argsort(priority):
+            out.append(array[p])
+        return out
+    else:
+        return array
+
+
+def indexed(name, shape, index=[iv, ix, iy, iz], velocities=None,
+            velocities_index=None, priority=None):
+    """
+    Return a SymPy matrix or an expression of indexed
+    objects.
+
+    Parameters
+    ----------
+
+    name : str
+        name of the SymPy IndexedBase
+
+    shape : list
+        shape of the SymPy IndexedBase
+
+    index : list
+        indices of the indexed object
+        (default is [iv, ix, iy, iz])
+
+    velocities : list
+        apply this list of integers on the space indices
+        (default is None)
+
+    velocities_index : list
+        list of velocities used in the indexed output
+        (default is None)
+
+    priority : list
+        define how to reorder the indeices (lower to greater)
+        (default is None)
+
+    Return
+    ------
+
+    sympy.Matrix or sympy.IndexedBase
+
+    Examples
+    --------
+
+    >>> import sympy as sp
+    >>> i, j, k = sp.symbols('i, j, k')
+    >>> m = indexed("m", [10, 100, 200], [i, j, k])
+    >>> m
+    m[i, j, k]
+    >>> m.shape
+    (10, 100, 200)
+
+    >>> m = indexed("m", [10, 100, 200], [i, j, k], velocities_index=range(4))
+    >>> m
+    Matrix([
+    [m[0, j, k]],
+    [m[1, j, k]],
+    [m[2, j, k]],
+    [m[3, j, k]]])
+
+    >>> m = indexed("m", [10, 100, 200], [i, j, k], velocities_index=range(4), priority=[1, 2, 0])
+    >>> m
+    Matrix([
+    [m[k, 0, j]],
+    [m[k, 1, j]],
+    [m[k, 2, j]],
+    [m[k, 3, j]]])
+
+    >>> m = indexed("m", [10, 100, 200], [i, j, k], velocities=[[0, 0], [1, 0], [-1, -1]])
+    >>> m
+    Matrix([
+    [        m[0, j, k]],
+    [    m[1, j + 1, k]],
+    [m[2, j - 1, k - 1]]])
+
+    """
+    if velocities_index and velocities:
+        raise ValueError("velocities and velocities_index can't be defined together.")
+
+    output = sp.IndexedBase(name, set_order(shape, priority))
+
+    if velocities_index:
+        ind = [set_order([k] + list(index[1:]), priority) for k in velocities_index]
         return sp.Matrix([output[i] for i in ind])
-    elif list_ind is not None:
+    elif velocities is not None:
         ind = []
         indices = index[1:]
-        for il, l in enumerate(list_ind): #pylint: disable=invalid-name
+        for iv, v in enumerate(velocities): #pylint: disable=invalid-name
             tmp_ind = []
-            for ik, k in enumerate(l): #pylint: disable=invalid-name
+            for ik, k in enumerate(v): #pylint: disable=invalid-name
                 tmp_ind.append(indices[ik] + int(k))
-            ind.append(set_order([il] + tmp_ind, permutation, remove_ind))
+            ind.append(set_order([iv] + tmp_ind, priority))
         return sp.Matrix([output[i] for i in ind])
     else:
-        return output[set_order(index, permutation, remove_ind)]
+        return output[set_order(index, priority)]
 
-def space_loop(ranges, permutation=None):
-    if not permutation:
-        permutation = range(len(ranges) + 1)
 
+def space_idx(ranges, priority=None):
+    """
+    Return a list of SymPy Idx with the bounds of the ranges.
+    This list can be permuted if priority is defined.
+
+    Parameters
+    ----------
+
+    ranges : list
+        bounds of the range for each created SymPy Idx
+
+    priority : list
+        define how to reorder the ranges (lower to greater)
+            (default is None)
+
+    Return
+    ------
+
+    list
+        list of SymPy Idx with the right ranges ordered by priority
+
+    Examples
+    --------
+
+    >>> loop = space_idx([(0, 10), (-10, 10)])
+    >>> loop
+    [ix_, iy_]
+    >>> loop[0].lower, loop[0].upper
+    (0, 10)
+    >>> loop = space_idx([(0, 10), (-10, 10)], priority=[1, 0])
+    >>> loop
+    [iy_, ix_]
+    >>> loop[0].lower, loop[0].upper
+    (-10, 10)
+
+    """
     indices = [ix_, iy_, iz_]
+
     idx = []
     for ir, r in enumerate(ranges): #pylint: disable=invalid-name
         idx.append(sp.Idx(indices[ir], r))
-    return set_order([0] + idx, permutation, remove_ind=[0])
+
+    if priority:
+        return set_order(idx, priority)
+    else:
+        return idx
 
 def alltogether(M, nsimplify=False):
     """
