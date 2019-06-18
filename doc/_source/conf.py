1d272a6df5992d5e3975b748545fdc211238718e
gouarin <loic.gouarin@gmail.com>
2019-06-18 12:09:48 +0200

    add new logos

--- doc/_source/conf.py
+++ doc/_source/conf.py
@@ -157,12 +157,12 @@ pngmath_dvipng_args = ['-gamma', '1.5', '-D', '96', '-bg', 'Transparent']
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "../_static/img/favicon.png"
+html_favicon = "../_theme/pylbm/static/img/pylbm.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['../_static']
+# html_static_path = ['../_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
