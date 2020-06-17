61c31f12010011c0c28b3e0a302c80706f60ba36
Graille <benjamin.graille@math.u-psud.fr>
2020-06-17 07:16:50 +0200

    heat equation with hyperbolic scaling

--- demo/1D/heat.py
+++ demo/1D/heat.py
@@ -7,11 +7,11 @@
 # License: BSD 3 clause
 
 """
- Solvers D1Q2 and D1Q3 for the advection equation on the 1D-torus
+ Solvers D1Q2 and D1Q3 for the heat equation on (0,1)
 
- d_t(u) + c d_x(u) = 0, t > 0, 0 < x < 1, (c=1/4)
+ d_t(u) = mu d_xx(u), t > 0, 0 < x < 1,
  u(t=0,x) = u0(x),
- u(t,x=0) = u(t,x=1)
+ u(t,x=0) = u(t,x=1) = 0
 """
 
 import sympy as sp
@@ -21,53 +21,21 @@ import pylbm
 # pylint: disable=redefined-outer-name
 
 U, X = sp.symbols('u, X')
-C, LA = sp.symbols('c, lambda', constants=True)
+MU, LA = sp.symbols('mu, lambda', constants=True)
 SIGMA_0, SIGMA_1, SIGMA_2 = sp.symbols(
     'sigma_0, sigma_1, sigma_2', constants=True
 )
 
 
-def u_init(x, xmin, xmax, reg):
+def solution(x, t, xmin, xmax):
     """
-    initial condition
+    solution as a eigenvector
     """
-    middle, width = .75*xmin+.25*xmax, 0.125*(xmax-xmin)
-    x_left, x_right = middle-width, middle+width
-    output = np.zeros(x.shape)
-
-    ind_l = np.where(np.logical_and(x > x_left, x <= middle))
-    ind_r = np.where(np.logical_and(x < x_right, x > middle))
-    x_sl = (x[ind_l] - x_left - 0.5*width) / (0.5*width)
-    x_sl_k = np.copy(x_sl)
-    x_sl *= x_sl
-    x_sr = (x[ind_r] - middle - 0.5*width) / (0.5*width)
-    x_sr_k = np.copy(x_sr)
-    x_sr *= x_sr
-
-    def binomial(n, k):
-        if 0 <= k <= n:
-            ntok = 1
-            ktok = 1
-            for t in range(1, min(k, n - k) + 1):
-                ntok *= n
-                ktok *= t
-                n -= 1
-            return ntok // ktok
-        else:
-            return 0
-
-    cte = 0.
-    for k in range(reg+1):
-        coeff = (-1)**k * binomial(reg, k) / (2*k+1)
-        output[ind_l] += coeff * x_sl_k
-        output[ind_r] -= coeff * x_sr_k
-        cte += coeff
-        x_sl_k *= x_sl
-        x_sr_k *= x_sr
-    output[ind_l] += cte
-    output[ind_r] += cte
-    output /= 2*cte
-    return output
+    return np.sin(
+        np.pi*(x-xmin)/(xmax-xmin)
+    )*np.exp(
+        -(np.pi/(xmax-xmin))**2*thermal_diffusivity*t
+    )
 
 
 def run(space_step,
@@ -106,9 +74,9 @@ def run(space_step,
     reg = 0                     # regularity of the initial condition
     xmin, xmax = 0., 1.         # bounds of the domain
     la = 1.                     # lattice velocity (la = dx/dt)
-    velocity = 0.25             # velocity of the advection
-    s_0 = 2.                    # relaxation parameter for the D1Q2
-    s_1, s_2 = 1.4, 1.0         # relaxation parameter for the D1Q3
+    sigma_0 = thermal_diffusivity
+    s_0 = 1./(.5+sigma_0)       # relaxation parameter for the D1Q2
+    s_1, s_2 = s_0, 1.0         # relaxation parameter for the D1Q3
 
     symb_s0 = 1/(0.5+SIGMA_0)   # symbolic relaxation parameter
     symb_s1 = 1/(0.5+SIGMA_1)   # symbolic relaxation parameter
@@ -119,7 +87,7 @@ def run(space_step,
 
     # dictionary of the simulation for the D1Q2
     simu_cfg_d1q2 = {
-        'box': {'x': [xmin, xmax], 'label': -1},
+        'box': {'x': [xmin, xmax], 'label': 0},
         'space_step': space_step,
         'scheme_velocity': LA,
         'schemes': [
@@ -128,22 +96,25 @@ def run(space_step,
                 'conserved_moments': U,
                 'polynomials': [1, X],
                 'relaxation_parameters': [0., symb_s0],
-                'equilibrium': [U, C*U],
+                'equilibrium': [U, 0],
             },
         ],
-        'init': {U: (u_init, (xmin, xmax, reg))},
+        # 'init': {U: (u_init, (xmin, xmax, reg))},
+        'init': {U: (solution, (0, xmin, xmax))},
         'generator': generator,
         'parameters': {
-            LA: la,
-            C: velocity,
+            LA: la/space_step,
             SIGMA_0: 1/s_0-.5
+        },
+        'boundary_conditions': {
+            0: {'method': {0: pylbm.bc.BouzidiAntiBounceBack}},
         },
         'show_code': False,
     }
 
     # dictionary of the simulation for the D1Q3
     simu_cfg_d1q3 = {
-        'box': {'x': [xmin, xmax], 'label': -1},
+        'box': {'x': [xmin, xmax], 'label': 0},
         'space_step': space_step,
         'scheme_velocity': LA,
         'schemes': [
@@ -152,18 +123,20 @@ def run(space_step,
                 'conserved_moments': U,
                 'polynomials': [1, X, X**2/2],
                 'relaxation_parameters': [0., symb_s1, symb_s2],
-                'equilibrium': [U, C*U, C**2*U/2],
+                'equilibrium': [U, 0, LA**2*U/2],
             },
         ],
-        'init': {U: (u_init, (xmin, xmax, reg))},
+        # 'init': {U: (u_init, (xmin, xmax, reg))},
+        'init': {U: (solution, (0, xmin, xmax))},
+        'boundary_conditions': {
+            0: {'method': {0: pylbm.bc.BouzidiAntiBounceBack}},
+        },
         'generator': generator,
         'parameters': {
-            LA: la,
-            C: velocity,
+            LA: la/space_step,
             SIGMA_1: 1/s_1-.5,
             SIGMA_2: 1/s_2-.5
         },
-        'relative_velocity': [C],
         'show_code': False,
     }
 
@@ -182,12 +155,20 @@ def run(space_step,
         x_d1q2 = sol_d1q2.domain.x
         l1a = axe.CurveScatter(
             x_d1q2, sol_d1q2.m[U],
-            color='navy', label=r'$D_1Q_2$'
+            color='navy', size=25,
+            label=r'$D_1Q_2$'
         )
         x_d1q3 = sol_d1q3.domain.x
         l1b = axe.CurveScatter(
             x_d1q3, sol_d1q3.m[U],
-            color='orange', label=r'$D_1Q_3$'
+            color='orange', size=25,
+            label=r'$D_1Q_3$'
+        )
+        x_exact = np.linspace(xmin, xmax, 1025)
+        l1e = axe.CurveLine(
+            x_exact, solution(x_exact, sol_d1q2.t, xmin, xmax),
+            color='black', alpha=0.5, width=2,
+            label='exact'
         )
         axe.legend(loc='upper right',
                    shadow=False,
@@ -196,13 +177,15 @@ def run(space_step,
 
         def update(iframe):  # pylint: disable=unused-argument
             # increment the solution of one time step
-            if sol_d1q2.t < final_time:
+            loc_tf = min(final_time, final_time*iframe/10)
+            while sol_d1q2.t < loc_tf:
                 sol_d1q2.one_time_step()
                 l1a.update(sol_d1q2.m[U])
-            if sol_d1q3.t < final_time:
+            while sol_d1q3.t < loc_tf:
                 sol_d1q3.one_time_step()
                 l1b.update(sol_d1q3.m[U])
-            axe.title = r'advection at $t = {0:f}$'.format(sol_d1q2.t)
+            l1e.update(solution(x_exact, sol_d1q2.t, xmin, xmax))
+            axe.title = r'heat at $t = {0:f}$'.format(sol_d1q3.t)
 
         fig.animate(update)
         fig.show()
@@ -215,6 +198,7 @@ def run(space_step,
 
 if __name__ == '__main__':
     # pylint: disable=invalid-name
-    space_step = 1./256
-    final_time = 2.
+    space_step = 1./32
+    thermal_diffusivity = 0.1
+    final_time = 1.
     solution = run(space_step, final_time)
